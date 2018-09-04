365ceeea3cff3d91fc4bbbeecf576eba43208fad
Masafumi Mochizuki <there_is_a_moon@yahoo.co.jp>
2018-09-05 02:49:41 +0900

    add_default_likes_count_to_blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180817151225) do
+ActiveRecord::Schema.define(version: 20180904174422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180817151225) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "likes_count"
+    t.integer "likes_count", default: 0
   end
 
   create_table "likes", force: :cascade do |t|
