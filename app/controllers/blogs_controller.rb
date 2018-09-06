7f86645dddc05cc65050c002b24e24e96d91d779
Masafumi Mochizuki <there_is_a_moon@yahoo.co.jp>
2018-09-07 01:08:28 +0900

    modify blogs_controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   before_action :require_login, only: [:new, :create, :edit, :show, :update, :destroy]
-  before_action :set_blog, only: [:edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   def show
   end
@@ -48,6 +48,7 @@ class BlogsController < ApplicationController
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
+
   def set_blog
     @blog = Blog.find(params[:id])
   end
